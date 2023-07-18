dae6a6094d936e3baac5fe40d326b67561ca3f0c
yassinht <yassinhtt@gmail.com>
2023-07-18 13:20:19 +0200

    code optimization

--- bankManagement/src/main/java/com/houta/bankManagement/models/AbstractEntity.java
+++ bankManagement/src/main/java/com/houta/bankManagement/models/AbstractEntity.java
@@ -3,24 +3,34 @@ package com.houta.bankManagement.models;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Data
 @SuperBuilder
 @NoArgsConstructor
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class AbstractEntity {
 
     @Id
     @GeneratedValue
     private Integer id;
 
+    @CreatedDate
+    @Column(
+            name = "createdDate",
+            nullable = false,
+            updatable = false
+    )
     private LocalDateTime creationDate;
 
-    private LocalDateTime lastUpdated;
+    @LastModifiedDate
+    @Column(name="lastModifiedDate")
+    private LocalDateTime lastModifiedDate;
 
 }
