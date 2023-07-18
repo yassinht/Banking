330044bbbaec4c7a18824e55de87bf7dcfb8f272
yassinht <yassinhtt@gmail.com>
2023-07-18 18:16:32 +0200

    query & native sql

--- bankManagement/src/main/java/com/houta/bankManagement/repositories/UserRepository.java
+++ bankManagement/src/main/java/com/houta/bankManagement/repositories/UserRepository.java
@@ -2,24 +2,38 @@ package com.houta.bankManagement.repositories;
 
 import com.houta.bankManagement.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface UserRepository extends JpaRepository<User,Integer> {
 
-    // select * from User where firstname = 'yassine
+    // select * from _user where firstname = 'yassine
     List<User> findAllByFirstname(String firstname);
 
-    // select * from User like firstname = 'yassine
+    // select * fromu _user like firstname = '%yassine%'
     List<User>findByFirstnameContaining(String firsname);
 
-    // select * from User ilike firstname = 'yassine
+    // select * from _user ilike firstname = 'yassine
     List<User>findByFirstnameContainingIgnoreCase(String firsname);
 
-    //select * from user u inner join account a on u.id = a.id_user and a.iban ='ibanexemple'
+    //select * from _user u inner join account a on u.id = a.id_user and a.iban ='ibanexemple'
     List<User>findAllByAccountIban( String iban);
 
-    //select * from user where firstname = 'yassine45' and email ='yassinhtt@gmail.com'
+    //select * from _user where firstname = 'yassine45' and email ='yassinhtt@gmail.com'
     User findByFirstnameContainingIgnoreCaseAndEmail(String firsname, String email);
+
+    @Query("from User where firstname= :fn")
+    List<User> searchByFirstname( @Param("fn") String firstname);
+
+    @Query("from User where firstname= '%:firstname%'")
+    List<User> searchByFirstnameContaining(  String firstname);
+
+    @Query("from User u inner join Account a on u.id = a.user.id where a.iban = :iban")
+    List<User> searchByByIban( String iban);
+
+    @Query(value = " select * from _user u inner join Account a on u.id = a.user.id where a.iban = :iban",nativeQuery = true)
+    List<User> searchByIbanNative(String Iban);
 
 }
